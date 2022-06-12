2c970d140731c0b2cc45606f8d94edaa2e824710
Prachi-Thakre <83527890+Prachi-Thakre@users.noreply.github.com>
2022-06-12 16:46:30 +0530

    mongodb_setup

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+
+	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+	clientOptions := options.Client().
+		ApplyURI("mongodb+srv://admin:admin@cluster0.ikqfb.mongodb.net/?retryWrites=true&w=majority").
+		SetServerAPIOptions(serverAPIOptions)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	usersCollection := client.Database("testing").Collection("users")
+	user := bson.D{{"fullName", "User 1"}, {"age", 30}}
+	// insert the bson object using InsertOne()
+	result, err := usersCollection.InsertOne(context.TODO(), user)
+	// check for errors in the insertion
+	if err != nil {
+		panic(err)
+	}
+	// display the id of the newly inserted object
+	fmt.Println(result.InsertedID)
+
+}
